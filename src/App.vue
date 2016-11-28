26a5f2a4eb5e3cde307c1f7ae551280fd4a5b4dc
Evgeny Zislis <evgeny.zislis@gmail.com>
2016-11-28 18:24:51 +0200

    add a login screen

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <hello></hello>
+    <Login></Login>
   </div>
 </template>
 
 <script>
-import Hello from './components/Hello';
+import Login from './components/Login';
 
 export default {
   name: 'app',
   components: {
-    Hello,
+    Login,
   },
 };
 </script>
