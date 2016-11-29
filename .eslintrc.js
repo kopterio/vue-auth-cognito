3dd7e576e18847106bd3e42af31ab2318f5cfbce
Evgeny Zislis <evgeny.zislis@gmail.com>
2016-11-29 22:18:24 +0200

    ignore extraneous-dependencies eslint warning for tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,6 +28,9 @@ module.exports = {
       'js': 'never',
       'vue': 'never'
     }],
+    'import/no-extraneous-dependencies': ['error', {
+      'devDependencies': [ '**/*.test.js' ]
+    }],
     'no-param-reassign': ['error', { props: false }],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
