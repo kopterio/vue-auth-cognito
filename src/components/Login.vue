f74783e5cd8be52b7fb9ec664cf769739f78a18a
Evgeny Zislis <evgeny.zislis@gmail.com>
2016-11-30 11:51:55 +0200

    cleanup login component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,32 +1,29 @@
 <template>
 <div class="container">
-    <div class="row">
-      <div class="col-md-4 col-md-offset-4">
-        <div class="panel panel-default">
-          <div class="panel-heading">
-            <h3 class="panel-title">Login via site</h3>
-         </div>
-          <div class="panel-body">
-            <form accept-charset="UTF-8" role="form">
-                    <fieldset>
-                <div class="form-group">
-                  <input class="form-control" placeholder="yourmail@example.com" name="email" type="text">
+  <div class="row">
+    <div class="col-md-4 col-md-offset-4">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h3 class="panel-title">Login</h3>
+        </div>
+        <div class="panel-body">
+          <form accept-charset="UTF-8" role="form">
+            <fieldset>
+              <div class="form-group">
+                <input class="form-control" placeholder="yourmail@example.com" name="email" type="text" :value="username">
               </div>
               <div class="form-group">
-                <input class="form-control" placeholder="Password" name="password" type="password" value="">
+                <input class="form-control" placeholder="Password" name="password" type="password" :value="password">
               </div>
               <div class="checkbox">
-                  <label>
-                    <input name="remember" type="checkbox" value="Remember Me"> Remember Me
-                  </label>
-                </div>
+                <label>
+                  <input name="remember" type="checkbox" value="Remember Me"> Remember Me
+                </label>
+              </div>
               <input class="btn btn-lg btn-success btn-block" type="submit" value="Login">
             </fieldset>
-              </form>
-                      <hr/>
-                    <center><h4>OR</h4></center>
-                    <input class="btn btn-lg btn-facebook btn-block" type="submit" value="Login via facebook">
-          </div>
+          </form>
+        </div>
       </div>
     </div>
   </div>
@@ -36,46 +33,12 @@
 <script>
 export default {
   name: 'Login',
+  data: {
+    username: 'yourmail@example.com',
+    password: 'VerySecret123!',
+  },
 };
 </script>
 
 <style>
-.white{
-    color:#000;
-    background-color:#fff;
-}
-
-.btn-facebook {
-    color: #ffffff;
-    -webkit-text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.25);
-    text-shadow: 0 -1px 0 rgba(0, 0, 0, 0.25);
-    background-color: #2b4b90;
-    *background-color: #133783;
-    background-image: -moz-linear-gradient(top, #3b5998, #133783);
-    background-image: -webkit-gradient(linear, 0 0, 0 100%, from(#3b5998), to(#133783));
-    background-image: -webkit-linear-gradient(top, #3b5998, #133783);
-    background-image: -o-linear-gradient(top, #3b5998, #133783);
-    background-image: linear-gradient(to bottom, #3b5998, #133783);
-    background-repeat: repeat-x;
-    border-color: #133783 #133783 #091b40;
-    border-color: rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.1) rgba(0, 0, 0, 0.25);
-    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#ff3b5998', endColorstr='#ff133783', GradientType=0);
-    filter: progid:DXImageTransform.Microsoft.gradient(enabled=false);
-}
-
-    .btn-facebook:hover,
-    .btn-facebook:focus,
-    .btn-facebook:active,
-    .btn-facebook.active,
-    .btn-facebook.disabled,
-    .btn-facebook[disabled] {
-        color: #ffffff;
-        background-color: #133783 !important;
-        *background-color: #102e6d !important;
-    }
-
-    .btn-facebook:active,
-    .btn-facebook.active {
-        background-color: #0d2456 \9 !important;
-    }
 </style>
