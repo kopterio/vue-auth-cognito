5f7e1f055c879498e110f417a08ef51b969ad46c
Anton Sekatski <antonsekatski@gmail.com>
2016-12-01 14:20:36 +0300

    Login component

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,21 +6,30 @@
         <div class="panel-heading">
           <h3 class="panel-title">Login</h3>
         </div>
+        <div class="alert alert-success" v-show="successMessage">
+          {{ successMessage }}
+        </div>
+        <div class="alert alert-danger" v-show="failure">
+          {{ failure }}
+        </div>
         <div class="panel-body">
-          <form accept-charset="UTF-8" role="form">
+          <form accept-charset="UTF-8" role="form" @submit.stop.prevent="handleSubmit">
             <fieldset>
               <div class="form-group">
-                <input class="form-control" placeholder="yourmail@example.com" name="email" type="text" v-model="username">
+                <input class="form-control" placeholder="testusername" name="username" type="text" v-model="username" required min="1" :disabled="disableAllInputs">
               </div>
               <div class="form-group">
-                <input class="form-control" placeholder="Password" name="password" type="password" v-model="password">
+                <input class="form-control" placeholder="Password" name="password" type="password" v-model="password" required min="6" pattern="[\S]+" :disabled="disableAllInputs">
+                <p class="form-text text-muted">
+                  Your password must be at least 6 characters long, contain letters and numbers and special symbols, and must not contain spaces.
+                </p>
               </div>
-              <div class="checkbox">
+              <!--<div class="checkbox">
                 <label>
                   <input name="remember" type="checkbox" value="Remember Me"> Remember Me
                 </label>
-              </div>
-              <input class="btn btn-lg btn-success btn-block" type="submit" value="Login">
+              </div>-->
+              <input class="btn btn-lg btn-success btn-block" type="submit" value="Login" :disabled="protectedUI || !formIsValid || disableAllInputs">
             </fieldset>
           </form>
         </div>
@@ -31,12 +40,42 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   name: 'Login',
   data: () => ({
+    successMessage: null,
+    disableAllInputs: false,
+    protectedUI: false,
     username: '',
     password: '',
   }),
+  methods: {
+    handleSubmit() {
+      this.protectedUI = true;
+      this.$store.dispatch('authenticateUser', {
+        username: this.username,
+        password: this.password,
+      }).then(() => {
+        this.disableAllInputs = true;
+        this.successMessage = 'Successfuly signed in';
+      }).catch(() => { this.protectedUI = false; });
+    },
+  },
+  computed: {
+    ...mapGetters([
+      'failure',
+    ]),
+    formIsValid() {
+      return /[\S]+/.test(this.username)
+      && /[a-z]+/.test(this.password)
+      && /[A-Z]+/.test(this.password)
+      && /[0-9]+/.test(this.password)
+      && /[!@#$%^&*()_+={}[\]\\;:.,|]+/.test(this.password)
+      && this.password.length >= 6;
+    },
+  },
 };
 </script>
 
