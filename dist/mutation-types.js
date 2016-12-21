bd7a53b85bbf2bcd1ad435807aba57193aba1958
Anton Sekatski <antonsekatski@gmail.com>
2016-12-21 11:31:25 +0300

    add dist folder to .gitignore

--- dist/mutation-types.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var prefix = 'vuex-cognito-auth';
-
-var AUTHENTICATE = exports.AUTHENTICATE = prefix + '/AUTHENTICATE';
-var SIGNOUT = exports.SIGNOUT = prefix + '/SIGNOUT';
